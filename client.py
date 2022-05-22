34ffe77dbfc72d119b547683fcc8c8c4bf5e1675
Cesar Cisneros <cisneros627@gmail.com>
2022-05-22 16:30:54 -0700

    this is the branch test commit

--- client.py
+++ client.py
@@ -3,4 +3,13 @@
 
 
 
-#Also I would like to know if this information is being routed to the local system. 
+# I need to use sockets to connect and test the back and forth between this file and the server file. 
+
+#the classes in this file  will feed into the main.py file so that interaction can happen. 
+
+# im a bit out of practice but I will get the hang of it, i think. 
+
+#for now you have to go back and keep going on your "my stacks" app
+
+#SO GO DO THAT NOW PLEASE!!!!
+
