e7f34bf26921950c9a514f2ce6c9e471ec2c72a4
Cesar <cisneros627@gmail.com>
2022-05-22 16:06:45 -0700

    Update client.py

--- client.py
+++ client.py
@@ -1,2 +1,6 @@
 #Here is the class that will be delivered to the player.
 #This software will need to be execuatable and safe for networking paramaters.
+
+
+
+#Also I would like to know if this information is being routed to the local system. 
